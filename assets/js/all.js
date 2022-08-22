ce0fd64fd4052b834ecf263ff9321affb342e18f
JHOUJHOU <a01450887@gmail.com>
2022-08-23 00:44:16 +0800

    Update 2022-08-22T16:44:11.966Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,31 +1,33 @@
+"use strict";
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 16,
   breakpoints: {
     768: {
-        slidesPerView: 3
+      slidesPerView: 3
     },
     992: {
-        slidesPerView: 10
+      slidesPerView: 10
     }
-  },
-  // autoplay: {
+  } // autoplay: {
   //   disableOnInteraction: false,
   //   delay: 0
   // },
   // speed: 1500,
   // loop: true,
-});
 
+});
 var superpowerswiper = new Swiper(".superpowerSwiper", {
   cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination"
   },
   mousewheel: true,
-  keyboard: true,
+  keyboard: true
 });
+//# sourceMappingURL=all.js.map
