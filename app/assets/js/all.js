4089f00c816d0bdc724ce987ba1c80a572d05c72
JHOUJHOU <a01450887@gmail.com>
2022-08-23 00:42:21 +0800

    作業完成

---
+++ app/assets/js/all.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+        slidesPerView: 3
+    },
+    992: {
+        slidesPerView: 10
+    }
+  },
+  // autoplay: {
+  //   disableOnInteraction: false,
+  //   delay: 0
+  // },
+  // speed: 1500,
+  // loop: true,
+});
+
+var superpowerswiper = new Swiper(".superpowerSwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
